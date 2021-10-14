e04c8559b12140b3ea575adadf72f1e73f1ef27c
hyunahpark <pha5763@gmail.com>
2021-10-14 19:37:11 +0900

    myMall 28

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Data from "./data";
 import Detail from './Detail.js';
 import Navbarjs from './Navbar.js';
 import { Navbar,Container,Nav,NavDropdown,Button } from 'react-bootstrap';
+import axios from 'axios';
 
 import { Link, Route, Switch } from 'react-router-dom';
 
@@ -11,42 +12,58 @@ import { Link, Route, Switch } from 'react-router-dom';
 
 function App() {
 
-  let [data, data변경함수] = useState([Data]);
+  let [data, data변경함수] = useState(Data);
+  let [재고, 재고변경함수] = useState([10,11,12]);
 
   return (
   <div className="App">
     <Navbarjs />
     
   <Switch>
-  
   <Route exact path="/">
     <div className="intro">
       <div className="intro_text">
-      <h1>20% season offf</h1>
-      <p>
-      This is a simple hero unit, a simple jumbotron-style component for calling extra attention to
-      featured content or information.
-      </p>
-      <p>
-      <Button variant="primary">Learn more</Button>
-      </p>
+        <h1>20% season offf</h1>
+        <p>
+        This is a simple hero unit, a simple jumbotron-style component for calling extra attention to
+        featured content or information.
+        </p>
+        <p>
+        <Button variant="primary">Learn more</Button>
+        </p>
       </div>
     </div>
 
     <div className="container">
-    <div className="row">
-      {
-        Data.map((a,i)=>{
-          return <Card Data={Data[i]} i={i} />
-        })
-      }
-      {/* Data 개수만큼 map을 돌려주세요 */}
+      <div className="row">
+        {
+          data.map((a,i)=>{
+            return <Card data={data[i]} i={i} />
+          })
+        }
+        {/* Data 개수만큼 map을 돌려주세요 */}
+      </div>
     </div>
-  </div>
+    <button className="btn btn-primary" onClick={()=>{ 
+
+      axios.post('서버URL', { id:'hyunah', pw: 1234 });
+      // 서버에 데이터를 보내고 싶을 때 POST 요청하는 법
+
+      // 성공하면 .then() 실패하면 .catch()
+      axios.get('https://codingapple1.github.io/shop/data2.json')
+      .then((result)=>{ 
+      
+        console.log(result.data);
+        data변경함수( [...data, ...result.data] ); // data 카피본 만들기
+       })
+      .catch(()=>{ 
+        console.log('ajax 요청 실패')
+       })
+     }}>더보기</button>
     </Route>
     
     <Route path="/detail/:id">
-        <Detail Data={Data}/>
+        <Detail Data={Data} 재고={재고} 재고변경함수={재고변경함수}/>
     </Route>
 
   </Switch>
@@ -61,8 +78,8 @@ function Card(props) {
   return (
     <div className="col-md-4">
       <img src={ 'https://codingapple1.github.io/shop/shoes' + (props.i +1) + '.jpg' } width="100%"  />
-      <h4>{ props.Data.title }</h4>
-      <p>{ props.Data.content }</p>
+      <h4>{ props.data.title }</h4>
+      <p>{ props.data.content }</p>
       </div>
       // Data라는 state는 부모컴포넌트가 가지고 있기 때문에 props 사용
   )
--- src/Detail.js
+++ src/Detail.js
@@ -1,18 +1,46 @@
-import React, { useState} from 'react';
+import React, { useEffect, useState} from 'react';
 import { useHistory, useParams } from 'react-router-dom';
+import styled from 'styled-components';
+import './Detail.scss';
+
+let 박스 = styled.div`
+    padding : 20px;
+`;
+
 
 function Detail(props) {
-
+    let [ alert, alert변경함수 ] = useState(true);
+    useEffect(()=>{
+        let 타이머 = setTimeout(()=>{ alert변경함수(false)},2000);
+        return ()=>{ clearTimeout(타이머) }
+    }, []);
 
     let { id } = useParams();
     let 찾은상품 = props.Data.find(function(상품){
         return 상품.id == id
     });
 
+    let 제목 = styled.h4`
+        font-size : 25px;
+        color : ${ props => props.색상 }
+    `;
+
     let history = useHistory(); //뒤로가기 버튼 만들기
 
     return (
       <div className="container">
+          <박스>
+            <제목 className="red">DETAIL</제목>
+          </박스>
+          
+          {
+              alert === true
+              ? (<div className="my-alert-yellow">
+                <p>재고가 얼마 남지 않았습니다!!</p>
+                </div>)
+            : null
+          }
+
       <div className="row">
         <div className="col-md-6">
           <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" />
@@ -21,12 +49,22 @@ function Detail(props) {
           <h4 className="pt-5">{ 찾은상품.title }</h4>
           <p>{ 찾은상품.content }</p>
           <p>{ 찾은상품.price }</p>
-          <button className="btn btn-danger">주문하기</button> 
+          <Info 재고={props.재고}></Info>
+
+          
+
+          <button className="btn btn-danger" onClick={ ()=>{ props.재고변경([9,10,11]) } }>주문하기</button> 
           <button className="btn btn-danger" onClick={ ()=>{ history.goBack();} }>뒤로가기</button> 
         </div>
       </div>
   </div> 
     )
+  }
+
+  function Info(props) {
+      return (
+          <p> 재고 : {props.재고[0]} </p>
+      )
   }
 
 export default Detail;
